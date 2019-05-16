003c6153a1d531a6d246947502cdc276b53c05f5
alexb24 <lakers76767@gmail.com>
2019-05-16 15:25:19 -0700

    created user functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import moment from "moment";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Alex React Redux Router</h1>
-        <div>
-          <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
-        </div>
-        <PortfolioContainer/>
+      <div className="container">
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug"component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,36 +1,71 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioItem from './portfolio-item';
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state ={
-            pageTitle: "Welcome to my portfolio",
-            data: [
-                {title: "Quip" },
-                {title: "Eventbrite" },
-                {title: "Ministry Safe" },
-                {title: "SwingAway" }
-            ]
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://alexbautista.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
+    return (
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("Top Dawg Entertainment")}>
+          Top Dawg Entertainment
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Tour Dates")}>
+          Tour Dates
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Merchandise")}>
+          Merchandise
+        </button>
 
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
-        })
-    }
-
-    render() {
-        return(
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                {this.portfolioItems()}
-            </div>
-        )
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
